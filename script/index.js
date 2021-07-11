13e210d53781f5299cc0b869b0b790a21e593492
Alinur <malyaev.alinur247@gmail.com>
2021-07-11 15:11:41 +0600

    fixed delete task feature

--- script/index.js
+++ script/index.js
@@ -1,39 +1,40 @@
-import {closeCreateTaskModal, openCreateTaskModal} from './modules/modals/createBtnModal.js'
-import {createElem} from './utiles.js'
+import { openCreateTaskModal, closeCreateTaskModal } from "./modules/modals/createBtnModal.js";
+import {createElem} from './utiles.js';
 import {openWarnModal, closeWarnModal} from './modules/modals/warnModal.js'
 
 const datas = JSON.parse(localStorage.getItem("data")) || [];
 
-console.log(datas);
+const taskBox = document.querySelector('.task__tasks');
+const createBtn = document.querySelector("#createBtn");
+const inputTitle = document.querySelector('.input__title');
+const inputDesc = document.querySelector('.input__desc');
+const errMsg = document.querySelector("#err__msg");
 
-const createModalCloseBtn = document.getElementById('create__modal__close');
-const createBtn = document.getElementById("createBtn");
-
-createModalCloseBtn.addEventListener("click", closeCreateTaskModal);
-createBtn.addEventListener("click", openCreateTaskModal);
-
-
-const errMsg = document.getElementById("err__msg");
-const saveBtn = document.querySelector(".create__save");
-
-const tasksBlock = document.querySelector(".task__tasks");
-const taskTitle = document.querySelector(".input__title");
-const taskDesc = document.querySelector(".input__desc");
+const saveBtn = document.querySelector('.create__save');
 
 
 function renderTasks(datas) {
-    tasksBlock.innerHTML = "";
-    datas.forEach((data, index) => {
-            let task = createElem("div", "taskNode");
-                let taskHeader = createElem("div", 'taskHeader');
-                    let title = createElem("h3", "task__h3", data.title);
-                    let taskCheckBox = createElem("input", "taskCheckBox");
-                    taskCheckBox.type = "checkbox";
-                    taskCheckBox.style.cursor = "pointer"
-                taskHeader.append(title, taskCheckBox);
+    taskBox.innerHTML = ''
+    let datasForRender = [...datas].filter(data => data.isForDelete === false);
+    datasForRender.forEach((data, index) => {
+        let task = createElem('div', "taskNode");
+            let taskHeader = createElem("div", 'taskHeader');
+            let title = createElem("h3", "task__h3", data.title);
+            let taskCheckBox = createElem("input", "taskCheckBox");
+            taskCheckBox.type = "checkbox";
+            taskCheckBox.style.cursor = "pointer"
+            taskHeader.append(title, taskCheckBox);
 
-            taskCheckBox.addEventListener("click", () => {
+            taskCheckBox.addEventListener('click', () => {
                 if(taskCheckBox.checked) {
+                    data.isChecked = true
+                    updateLocal();
+                }else {
+                    data.isChecked = false
+                    updateLocal();
+                }
+
+                if(data.isChecked === true){
                     task.style.opacity = 0.6;
                     task.style.transitionDuration = "0.3s";
                     title.style.textDecoration = "line-through"
@@ -43,75 +44,57 @@ function renderTasks(datas) {
                     title.style.textDecoration = "none"
                 }
             })
-
             let desc = createElem("p", "task__desc", data.desc);
             let removeTaskBtn = createElem("button", "removeTaskBtn");
             removeTaskBtn.style.backgroundImage = "url(../img/trush.png)";
-            task.append(taskHeader, desc, removeTaskBtn);
-
-            // removeTaskBtn.addEventListener("click", () => {openWarnModal(this.data)}
 
             removeTaskBtn.addEventListener("click", () => {
-                console.log(index);
-                deleteTask(index);
-                renderTasks(datas);
-            });
+                openWarnModal();
+                let delBtn = document.querySelector('.warn__btn_delete');
+                delBtn.addEventListener("click", () => {
+                    data.isForDelete = true
+                    updateLocal();
+                    console.log(data);
+                    renderTasks(datas);
+                    closeWarnModal();
+                })
+            })
 
-        tasksBlock.prepend(task);
-    })   
-}
+        task.append(taskHeader, desc, removeTaskBtn);
 
-function deleteTask(index) {
-    datas.splice(index, 1);
-    updateLocal();
+        taskBox.prepend(task)
+    })
 };
 
-function updateLocal() {
-    localStorage.setItem("data", JSON.stringify(datas));
-}
 
-renderTasks(datas);
+createBtn.addEventListener('click', openCreateTaskModal);
 
+saveBtn.addEventListener("click", () => {
+    if(inputTitle.value) {
+            let data = {
+                title: inputTitle.value,
+                desc: inputDesc.value,
+                isForDelete: false,
+                isChecked: false
+            };
 
-saveBtn.addEventListener("click", function() {
-
-        // if(taskTitle.value) {
-        //     taskTitleV = taskTitle.value 
-        //     taskTitle.style.border = 'none'
-        // }else {
-        //     taskTitle.style.border = '1px solid red';
-        //     errMsg.innerHTML = "Заголовок не задан";
-        //     errMsg.style.display = 'block'
-        // }
-
-        // if(taskDesc.value) {
-        //     taskDescV = taskDesc.value 
-        //     taskDesc.style.border = 'none'
-        // }else {
-        //     taskDesc.style.border = '1px solid red';
-        //     errMsg.innerHTML = "Описание пусто..";
-        //     errMsg.style.display = 'block'
-        // }
-    
-    let taskTitleV = taskTitle.value
-    let taskDescV = taskDesc.value;
-
-    let data = {
-        title: taskTitleV,
-        desc: taskDescV,
-        isForDelete: false
-    };
-
-
-    datas.push(data);
-    updateLocal();
-    clearInputs();
-    closeCreateTaskModal();
-    renderTasks(datas);
+            datas.unshift(data);
+            updateLocal();
+            renderTasks(datas);
+            clearInputs();
+            closeCreateTaskModal();
+    }else {
+        errMsg.innerHTML = "Нету заголовка..";
+        errMsg.style.color = 'red';
+        errMsg.style.display = 'block';
+    }
 })
 
-function clearInputs() {
-    taskTitle.value = "";
-    taskDesc.value = "";
-}
+function updateLocal() {
+    localStorage.setItem("data", JSON.stringify(datas))
+};
 
+function clearInputs() {
+    inputTitle.value = ""
+    inputDesc.value = ""
+}
